e7dec26815476d01836124e962ecbdbf79c47bf8
Sangeon <sp35@rice.edu>
2016-05-26 10:35:42 +0200

    17

--- Test1/python/test1_cfi.py
+++ Test1/python/test1_cfi.py
@@ -1,7 +1,7 @@
 import FWCore.ParameterSet.Config as cms
 
 demo = cms.EDAnalyzer('Test1',
-                     vertexSrc = cms.InputTag('offlinePrimaryVertices'),
+                     vertexSrc = cms.string('offlinePrimaryVertices'),
                      trackSrc = cms.InputTag('generalTracks'),
                      nHitCut = cms.int32(3),
                      dxySigCut = cms.double(1.0),
