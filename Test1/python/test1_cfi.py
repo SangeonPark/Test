ae8ed3eaa80729400dd522947a5dd4c3c7124f4e
Sangeon <sp35@rice.edu>
2016-05-27 12:22:56 +0200

    highmult

--- Test1/python/test1_cfi.py
+++ Test1/python/test1_cfi.py
@@ -1,12 +1,12 @@
 import FWCore.ParameterSet.Config as cms
 
 demo = cms.EDAnalyzer('Test1',
-                     vertexSrc = cms.string('offlinePrimaryVertices'),
-                     trackSrc = cms.InputTag('generalTracks'),
+                      vertexSrc = cms.string('offlinePrimaryVertices'),
+                      trackSrc = cms.InputTag('generalTracks'),
                     # nHitCut = cms.int32(3),
-                     dxySigCut = cms.double(3.0),
-                     dzSigCut = cms.double(3.0),
-                     etaCutMin = cms.double(-2.4),
+                      dxySigCut = cms.double(3.0),
+                      dzSigCut = cms.double(3.0),
+                      etaCutMin = cms.double(-2.4),
                       etaCutMax = cms.double(2.4),
                       NTrkMin = cms.int32(185),
                       NTrkMax = cms.int32(220)
