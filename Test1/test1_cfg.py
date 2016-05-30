8be5b9aff1acdedf9dfc0d7b802460b558dbc62f
Sangeon <sp35@rice.edu>
2016-05-30 11:06:00 +0200

    added comment

--- Test1/test1_cfg.py
+++ Test1/test1_cfg.py
@@ -1,7 +1,7 @@
 import FWCore.ParameterSet.Config as cms
 
 process = cms.Process("Demo")
-
+#eee
 process.load("FWCore.MessageService.MessageLogger_cfi")
 process.MessageLogger.cerr.FwkReport.reportEvery = cms.untracked.int32(1000)
 process.options   = cms.untracked.PSet( wantSummary = cms.untracked.bool(True) )
